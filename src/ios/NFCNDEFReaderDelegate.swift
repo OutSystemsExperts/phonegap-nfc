a9e85ba932ac02c01b1b0d9980d88f159b8b737c
André Gonçalves <andre.goncalves@outsystems.com>
2020-04-12 01:13:10 +0100

    no message

--- src/ios/NFCNDEFReaderDelegate.swift
+++ src/ios/NFCNDEFReaderDelegate.swift
@@ -1,102 +1,26 @@
 //
-//  NFCNDEFDelegate.swift
+//  NFCNDEFReaderDelegate.swift
 //  NFC
-//
-//  Created by dev@iotize.com on 05/08/2019.
-//  Copyright © 2019 dev@iotize.com. All rights reserved.
 //
 
 import Foundation
 import CoreNFC
 
-class NFCNDEFDelegate: NSObject, NFCNDEFReaderSessionDelegate {
+class NFCNDEFReaderDelegate: NSObject, NFCNDEFReaderSessionDelegate {
     var session: NFCNDEFReaderSession?
     var completed: ([AnyHashable : Any]?, Error?) -> ()
-    var textToWrite: String?
-    
-    init(completed: @escaping ([AnyHashable: Any]?, Error?) -> (), message: String?, textToWrite: String) {
-        self.completed = completed
-        self.textToWrite = textToWrite;
-        super.init()
-        initialize(completed: completed, message: message);
-    }
     
     init(completed: @escaping ([AnyHashable: Any]?, Error?) -> (), message: String?) {
         self.completed = completed
         super.init()
-        initialize(completed: completed, message: message);
-    }
-    
-    func initialize(completed: @escaping ([AnyHashable: Any]?, Error?) -> (), message: String?){
         
-        self.session = NFCNDEFReaderSession.init(delegate: self, queue: nil, invalidateAfterFirstRead: false)
+        self.session = NFCNDEFReaderSession.init(delegate: self, queue: nil, invalidateAfterFirstRead: true)
         if (self.session == nil) {
             self.completed(nil, "NFC is not available" as? Error);
             return
         }
         self.session!.alertMessage = message ?? ""
         self.session!.begin()
-    }
-    
-    @available(iOS 13.0, *)
-    func readerSession(_ session: NFCNDEFReaderSession, didDetect tags: [NFCNDEFTag]) {
-        if (textToWrite == ""){
-            return;
-        }
-        // 1
-           guard tags.count == 1 else {
-               session.invalidate(errorMessage: "Can not write to more than one tag.")
-               return
-           }
-           let currentTag = tags.first!
-           
-           // 2
-           session.connect(to: currentTag) { error in
-               
-               guard error == nil else {
-                   session.invalidate(errorMessage: "Could not connect to tag.")
-                   return
-               }
-               
-               // 3
-               currentTag.queryNDEFStatus { status, capacity, error in
-                   
-                   guard error == nil else {
-                       session.invalidate(errorMessage: "Could not query status of tag.")
-                       return
-                   }
-                   
-                   switch status {
-                   case .notSupported: session.invalidate(errorMessage: "Tag is not supported.")
-                   case .readOnly:     session.invalidate(errorMessage: "Tag is only readable.")
-                   case .readWrite:
-
-                    // 2
-                    let textPayload = NFCNDEFPayload.wellKnownTypeTextPayload(
-                        string: self.textToWrite ?? "no value passed",
-                        locale: Locale.init(identifier: "en")
-                    )!
-                    
-                    let messge = NFCNDEFMessage.init(
-                        records: [
-                            textPayload
-                        ]
-                    )
-                       // 4
-                       currentTag.writeNDEF(messge) { error in
-                           
-                           if error != nil {
-                               session.invalidate(errorMessage: "Failed to write message.")
-                           } else {
-                               session.alertMessage = "Successfully wrote data to tag!"
-                               session.invalidate()
-                           }
-                       }
-                       
-                   @unknown default:   session.invalidate(errorMessage: "Unknown status of tag.")
-                   }
-               }
-           }
     }
     
     func readerSession(_: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
@@ -118,9 +42,6 @@ class NFCNDEFDelegate: NSObject, NFCNDEFReaderSessionDelegate {
         let response = message.ndefMessageToJSON()
         completed(response, nil)
     }
-    
-    
-    
 }
 
 extension NFCNDEFMessage {
